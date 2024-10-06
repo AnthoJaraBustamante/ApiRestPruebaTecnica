9a0260c29f9234f2b33ffc154d58e3230bb9737d
AnthoJaraBustamante <omarbush81@gmail.com>
2024-10-06 01:24:58 -0500

    Agregar clases de datos ApiResponse, Product y Category para manejar respuestas de la API.

---
+++ app/src/main/java/com/anthojb/simple/apirestpruebatecnica/model/Product.kt
@@ -0,0 +1,18 @@
+data class ApiResponse (
+    val results: List<Product> = emptyList(),
+)
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val images: List<String>,
+    val category: Category
+)
+
+data class Category(
+    val id: Int,
+    val name: String,
+    val image: String
+)
